e4599b35bc803b4cdfd18555a12b8e30ba537d27
Alex Mills (Viper007Bond) <github@viper007bond.com>
2013-06-13 19:37:59 -0700

    Strip version number and add URL
    I don't want to have to keep the version number up to date. If you're
    using this, then you're smart enough to make sure you're using the
    latest version.

--- new-post-format-unmigrator.php
+++ new-post-format-unmigrator.php
@@ -3,8 +3,7 @@
 **************************************************************************
 
 Plugin Name:  New Post Format Unmigrator
-Plugin URI:   #
-Version:      1.0.0
+Plugin URI:   https://github.com/Viper007Bond/new-post-format-unmigrator
 Description:  If you were running WordPress trunk during the development of version 3.6, you may have used the new post format UI and it's extra media fields. Since those fields were yanked from core, this plugin migrates the data back into your post content so that it isn't lost in your post meta.
 Author:       Alex Mills (Viper007Bond)
 Author URI:   http://www.viper007bond.com/
